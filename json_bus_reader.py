a2f7a5c3ad74d5e13324322c29a27f7508c6c760
Avamaco <Maco_2003@wp.pl>
2024-01-23 23:25:40 +0100

    Updated json_bus_reader to group data by bus

--- json_bus_reader.py
+++ json_bus_reader.py
@@ -1,10 +1,30 @@
 import json
 import os
 
-in_file = open(os.path.join('collected_data', 'bus.json'), 'r')
-data = json.load(in_file)
-in_file.close()
-buses = data['result']
-for b in buses:
-    if b['Lines'] == '504':
-        print(b)
+
+def find_bus_routes(day):
+    dir_path = os.path.join('collected_data', day)
+    if not os.path.exists(dir_path):
+        print("Directory " + dir_path + "does not exist.")
+        return
+    bus_routes = {}
+    for file_name in os.listdir(dir_path):
+        in_file = open(os.path.join(dir_path, file_name), 'r')
+        data = json.load(in_file)['result']
+        in_file.close()
+        if not isinstance(data, list):
+            continue
+        for bus in data:
+            key = (bus['VehicleNumber'])
+            value = [bus['Lines'], bus['Time'], bus['Lon'], bus['Lat']]
+            bus_routes.setdefault(key, []).append(value)
+
+    out_dir = 'processed_data'
+    os.makedirs(out_dir, exist_ok=True)
+    out_file_path = os.path.join(out_dir, day + ".json")
+    out_file = open(out_file_path, 'w')
+    json.dump(bus_routes, out_file)
+    out_file.close()
+
+
+find_bus_routes('2024-01-22')
