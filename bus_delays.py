6bc593e600f006d578d33ab862069d87be8d4639
Avamaco <Maco_2003@wp.pl>
2024-02-18 23:09:25 +0100

    Added calculating delays

--- bus_delays.py
+++ bus_delays.py
@@ -1,5 +1,7 @@
 import os
 import time
+import json
+import datetime
 
 import pandas
 from api_data_collector import download_timetable
@@ -39,6 +41,41 @@ def get_arrivals():
     return df
 
 
+def get_times(busstop_id, busstop_nr, line):
+    file_name = busstop_id + "_" + busstop_nr + "_" + line + ".json"
+    in_file_path = os.path.join("collected_data", "timetables", file_name)
+    if not os.path.exists(in_file_path):
+        return None
+    in_file = open(in_file_path, 'r')
+    json_data = json.load(in_file)
+    in_file.close()
+    return [elem["values"][5]["value"] for elem in json_data["result"]]
+
+
+def find_delay(bus_time, timetable):
+    time1 = datetime.datetime.fromisoformat(bus_time)
+    day = bus_time[:11]
+    for i in reversed(range(len(timetable))):
+        if int(timetable[i][:2]) > 23:
+            timetable[i] = str(int(timetable[i][:2]) - 24).zfill(2) + timetable[i][2:]
+        time2 = datetime.datetime.fromisoformat(day + timetable[i])
+        delay = (time1 - time2).total_seconds()
+        if (delay > 0):
+            return delay
+    return -1
+
+
+def find_all_delays(df):
+    df["Delay"] = [find_delay(bus_time, get_times(busstop_id, str(busstop_nr).zfill(2), line))
+                   for busstop_id, busstop_nr, line, bus_time
+                   in df.values.tolist()]
+    return df.loc[df["Delay"] < 1200].loc[df["Delay"] > -1]
+
+
 if __name__ == "__main__":
     arrivals = get_arrivals()
-    download_required_timetables(arrivals)
+    #download_required_timetables(arrivals)
+    arrivals = find_all_delays(arrivals)
+    print(arrivals)
+    print(arrivals["Delay"].median())
+
