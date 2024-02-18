efc7f366ea5320538bd5332fe756e912f03c111b
Avamaco <Maco_2003@wp.pl>
2024-02-18 13:07:43 +0100

    Calculated closest bus stop positions for each bus

--- bus_stop_distances.py
+++ bus_stop_distances.py
@@ -3,6 +3,8 @@ from geopy import distance
 import pandas
 import os
 
+import create_dataframe
+
 
 def get_bus_stop_dict():
     in_file_path = os.path.join("collected_data", "bus_stops.json")
@@ -37,14 +39,18 @@ def get_route_lists():
 
 
 def get_closest_stops(lat, lon, line, bus_lists, stop_pos):
+    if line not in bus_lists.keys():
+        return None
     line_lists = bus_lists[line]
     distances = [[distance.distance(stop_pos[s], (lat, lon)).meters for s in route] for route in line_lists]
     closest_ids = [min(range(len(route) - 1), key=route.__getitem__) for route in distances]
-    return [(line_lists[i][closest_ids[i]], distances[i][closest_ids[i]],
-             line_lists[i][closest_ids[i] + 1], distances[i][closest_ids[i] + 1]) for i in range(len(line_lists))]
+    return [((line_lists[i][closest_ids[i]], distances[i][closest_ids[i]]),
+             (line_lists[i][closest_ids[i] + 1], distances[i][closest_ids[i] + 1])) for i in range(len(line_lists))]
 
 
 def predict_closest_stop(new_stops, old_stops):
+    if new_stops is None or old_stops is None or len(old_stops) != len(new_stops):
+        return None
     for i in range(len(new_stops)):
         if new_stops[i][0][0] == old_stops[i][1][0]:  # old next stop is now the closest
             return new_stops[i][0]
@@ -54,8 +60,21 @@ def predict_closest_stop(new_stops, old_stops):
     return None  # cannot predict the closest stop
 
 
-if __name__ == "__main__":
+def df_closest_stop(df):
     stops = get_bus_stop_dict()
     routes = get_route_lists()
-    my504 = routes["504"]
-    print(my504)
+    close_stops = pandas.Series([get_closest_stops(lat, lon, line, routes, stops)
+                                 for lat, lon, line in zip(df["Lat"], df["Lon"], df["Lines"])])
+    df["ClosestStop"] = [predict_closest_stop(new_stops, old_stops) if new_bus == old_bus else None
+                         for new_stops, old_stops, new_bus, old_bus
+                         in zip(close_stops, close_stops.shift(), df["VehicleNumber"], df["VehicleNumber"].shift())]
+    return df
+
+
+if __name__ == "__main__":
+    morning = create_dataframe.combine_into_df("2024-01-30", "10-00-07.csv", "11-00-59.csv")
+    print(len(morning))
+    closest_bus_stops = df_closest_stop(morning).dropna()
+    print(len(closest_bus_stops))
+    out_path = os.path.join("processed_data", "dist-2024-01-30.csv")
+    closest_bus_stops.to_csv(out_path)
