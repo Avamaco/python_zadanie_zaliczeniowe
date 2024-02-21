e095878869198cd22325e01a879703f84799c92a
Avamaco <Maco_2003@wp.pl>
2024-02-21 17:23:35 +0100

    Added a speed graph

--- zadanie_zaliczeniowe/warsaw_buses/bus_speed/visualize_speed.py
+++ zadanie_zaliczeniowe/warsaw_buses/bus_speed/visualize_speed.py
@@ -19,11 +19,25 @@ def show_speed_map(speed_data):
     fig.show()
 
 
+def show_speed_graph(speed_data):
+    speed_data = speed_data.loc[speed_data["Speed"] > 10]
+    avg_spd = speed_data.groupby("Lines")["Speed"].mean()
+    avg_df = pandas.DataFrame({
+        "Lines": speed_data["Lines"],
+        "AvgSpeed": [avg_spd.to_dict()[line] for line in speed_data["Lines"]]
+    }).drop_duplicates()
+    smallest_and_largest = pandas.concat((avg_df.nsmallest(5, "AvgSpeed"),
+                                          avg_df.nlargest(5, "AvgSpeed")))
+    fig = px.bar(smallest_and_largest, x='Lines', y='AvgSpeed')
+    fig.show()
+
+
 if __name__ == "__main__":
     morning = read_and_calc_data("2024-01-30", "10-00-07.csv", "11-00-59.csv")
     rush_hour = read_and_calc_data("2024-01-30", "15-03-46.csv", "16-03-09.csv")
     evening = read_and_calc_data("2024-01-30", "19-59-14.csv", "21-00-11.csv")
     all_data = pandas.concat((morning, rush_hour, evening))
+    show_speed_graph(all_data)
     show_speed_map(all_data)
     print("Liczba autobusów, które przekroczyły 50 km/h: " + str(len(all_data.loc[all_data["Speed"] > 50])))
     print("Szybkość średnia rano: " + str(morning["Speed"].mean()))
